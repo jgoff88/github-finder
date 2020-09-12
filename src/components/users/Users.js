eb48f86256fee7af335bb12c7c41c73615f17280
James Goff <james_goff88@outlook.com>
2020-09-12 18:18:58 -0500

    update app.js with axios api call

---
+++ src/components/users/Users.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return <div style={userStyle}>{users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
+	}
+};
+const userStyle = {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridgap: '1rem'
+};
+
+export default Users;
